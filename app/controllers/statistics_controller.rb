d2bb6070a5d26c80ffd76daa6ccc4656b4eb860c
= <=>
2012-03-14 01:21:25 -0400

    added totaling for statistics closes #1

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,6 +1,6 @@
 class StatisticsController < ApplicationController
   def index
-  	@tweets = Tweet.find(:all,:select=>"username, sentiment, count(id) as count",:group=>"sentiment,username")
+  	@tweets = Tweet.find(:all,:select=>"username, sentiment, count(id) as count",:group=>"sentiment,username", :order => "username")
   	@sentiment_values = { -1 => "Unsure", 0 => "Negative", 1 => "Positive" }
   end
 end
